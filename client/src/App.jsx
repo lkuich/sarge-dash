5a0c828dbf059e9a306c5200b8cee171ba9e086c
Loren Kuich <loren.jk3@gmail.com>
2023-04-11 09:58:56 -0700

    Init commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from 'primereact/button'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Button>Test</Button>
+    </div>
+  )
+}
+
+export default App
